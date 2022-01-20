433a2272d11aeb557ba15880a6f9fe4b7389999f
patrick mugambi <patrick@kali.pm.com>
2022-01-20 10:12:43 +0300

    create book model

--- Basi/models.py
+++ Basi/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+class BookModel(models.Model):
+    """[BookModel]
+
+    Args:
+        models ([Class]): [Book model to book a seat]
+
+    Returns:
+        [modelDatabase]: [Create table from python class]
+    """
+    user_requesting = models.ForeignKey(User, on_delete=models.CASCADE)
+    from_location = models.CharField(max_length=100, blank=True)
+    to_location = models.CharField(max_length=100, blank=True)
+    date_posted = models.DateTimeField(default=timezone.now)
+    
+    
+    def __str__(self):
+        return self.user_requesting + from_location
+
+
+    
