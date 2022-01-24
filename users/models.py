4b3f557766f06165d100a74f391d48f9df086bdf
patrick mugambi <patrick@kali.pm.com>
2022-01-24 13:54:42 +0300

    create user custom class and a user:driver class

--- users/models.py
+++ users/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+from django.utils import timezone
+import datetime
+class User(AbstractUser):
+    """[Abstract class]
+
+    Args:
+        AbstractUser ([class]): [Abstarct class to create two custom users]
+    """
+    is_passenger = models.BooleanField(default=False)
+    is_driver = models.BooleanField(default=False)
+
+class Driver(models.Model):
+    """[Driver model]
+
+    Args:
+        models ([class]): [Create a user[driver]]
+    """
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
