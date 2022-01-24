fc0b0d3c90da0f13d97fb4d3eebcc6ca91a3bef9
patrick mugambi <patrick@kali.pm.com>
2022-01-24 14:30:47 +0300

    add urls to handle user and homepage view

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
+from . import views
 app_name = "users"
 urlpatterns = [
+    path("", views.home, name="home"),
+    path("customer", views.customer, name="customer"),
+    path("driver", views.driver, name="driver"),
     
 ]
