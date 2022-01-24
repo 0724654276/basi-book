e94ef37b0cebf768361f2009ad68f04e04e26436
patrick mugambi <patrick@kali.pm.com>
2022-01-25 02:59:30 +0300

    make migrations and register busmodel in admin

--- users/admin.py
+++ users/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Driver,Passenger
+from .models import Driver,Passenger,BusModel
 # Register your models here.
 admin.site.register(Driver)
 admin.site.register(Passenger)
+admin.site.register(BusModel)
