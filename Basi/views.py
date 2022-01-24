478eff35c4e9101a87115889a255e4c43faf463e
patrick mugambi <patrick@kali.pm.com>
2022-01-24 14:41:28 +0300

    create homepage view"

--- Basi/views.py
+++ Basi/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+
+from django.shortcuts import redirect, render
+from django.urls import reverse, reverse_lazy
+from django.views.generic import TemplateView
 
 # Create your views here.
+
+def home(request):
+    """[home view]
+
+    Args:
+        request ([function]): [view function to handle homepage based on user authorization levels]
+
+    Returns:
+        [template]: [return homepage template]
+    """
+    if request.user.is_authenticated:
+        if request.user.is_customer:
+            return redirect(reverse_lazy('users:customer'))
+        else:
+            return redirect('users:driver')
+    return render(request, 'home/home.html')
