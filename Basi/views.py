332f441c55b114cdaac1d87446d7b989c9b6c91e
0724654276 <you@example.com>
2022-01-25 07:13:04 +0300

    create view function for homepage

--- Basi/views.py
+++ Basi/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+import requests
+from .models import Bus
 
 # Create your views here.
+def home(request):
+    if request.user.is_authenticated:
+        return render(request, 'index.html')
+    else:
+        return render(request, 'index.html')
+
+
+def findbus(request):
+    context = {}
+    if request.method == 'POST':
+        terminus_r = request.POST.get('terminus')
+        route_r = request.POST.get('route')
+        date_r = request.POST.get('date')
+        Bus_list = Bus.objects.filter(terminus=terminus_r, route=route_r, date=date_r)
+        if Bus_list:
+             return render(request, 'list.html', locals())
+        else:
+            context["error"] = "Sorry no buses available"
+            return render(request, 'findbus.html', context)
+    else:
+        return render(request, 'findbus.html')        
