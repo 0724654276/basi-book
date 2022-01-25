6240ce904b408516edb41fb690dbc5ba63925d06
patrick mugambi <patrick@kali.pm.com>
2022-01-25 12:24:31 +0300

    profile views

--- profileApp/views.py
+++ profileApp/views.py
@@ -1,3 +1,90 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render,redirect,get_object_or_404
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required 
+from .forms import UserUpdateForm, ProfileUpdateForm
+from users.models import BusModel
+from .models import Profile
+from users.models import User
+# Create your views here.
+
+# Update it here
+@login_required
+def profile(request):
+    images = BusModel.objects.all()
+    post = BusModel.objects.filter(author=request.user).order_by('-date_posted')
+    if request.method == 'POST':
+        u_form = UserUpdateForm(request.POST, instance=request.user)
+        p_form = ProfileUpdateForm(request.POST,
+                                   request.FILES,
+                                   instance=request.user.profile)
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+            messages.success(request, f'Your account has been updated!')
+            return redirect('Basi:index')
+
+    else:
+        u_form = UserUpdateForm(instance=request.user)
+        p_form = ProfileUpdateForm(instance=request.user.profile)
+
+    context = {
+        'u_form': u_form,
+        'p_form': p_form,
+        'images': images,
+        'posts' : post
+    }
+
+    return render(request, 'profile.html', context)
+
+
+
+
+
+@login_required
+def all_profiles(request):
+    images = BusModel.objects.all()
+    post = BusModel.objects.filter(author=request.user).order_by('-date_posted')
+    if request.method == 'POST':
+        u_form = UserUpdateForm(request.POST, instance=request.user)
+        p_form = ProfileUpdateForm(request.POST,
+                                   request.FILES,
+                                   instance=request.user.profile)
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+            messages.success(request, f'Your account has been updated!')
+            return redirect('homepage:index')
+
+    else:
+        u_form = UserUpdateForm(instance=request.user)
+        p_form = ProfileUpdateForm(instance=request.user.profile)
+
+    context = {
+        'u_form': u_form,
+        'p_form': p_form,
+        'images': images,
+        'posts' : post
+    }
+
+    return render(request, 'all_profiles.html', context)
+
+@login_required(login_url='login')
+def user_profile(request, username):
+    user_prof = get_object_or_404(User, username=username)
+    if request.user == user_prof:
+        return redirect('profile', username=request.user.username)
+    user_posts = user_prof.profile.posts.all()
+    
+   
+    params = {
+        'user_prof': user_prof,
+        'user_posts': user_posts,
+        
+    }
+  
+    return render(request, 'user_profile.html', params)
+
+
