6c25d0358f6eb738f54abc66b9a77aa7031dc1d5
synthiakageni <synthia.kageni@student.moringaschool.com>
2022-01-25 10:03:07 +0300

    update file

--- Bus/urls.py
+++ Bus/urls.py
@@ -23,7 +23,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", include("users.urls", namespace="users")),
     path("", include("Basi.urls", namespace="Basi")),
     path('accounts/', include('django.contrib.auth.urls')),
     
