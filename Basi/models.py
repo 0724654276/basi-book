241970a4e710c95712d38cbe787bb37a3bc2b937
synthiakageni <synthia.kageni@student.moringaschool.com>
2022-01-26 07:02:00 +0300

    create bus booking models

--- Basi/models.py
+++ Basi/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+
+class Busi(models.Model):
+    
+    bus = models.OneToOneField(Bus, on_delete=models.CASCADE)
+    image = models.ImageField(default='bus.jpg', upload_to='bus_pics')
+
+    def __str__(self):
+        return f'{self.bus.busname} Bus' 
+    
+    def save_profile(self):
+        super().save()
+
+    @classmethod
+    def get_bus(cls):
+        bus = Busi.objects.all()
+        return Busi
+
+    @classmethod
+    def find_bus(cls,search_term):
+        bus = Busi.objects.filter(bus__busname__icontains=search_term)
+        return bus
+
+        
