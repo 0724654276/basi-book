371505c0b8668211d510fa6d3ab29702ca711571
patrick mugambi <patrick@kali.pm.com>
2022-01-28 12:43:31 +0300

    set up newsletter subscription using mailchimp

--- Basi/models.py
+++ Basi/models.py
@@ -2,3 +2,16 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User 
 # Create your models here.
+class ContactUs(models.Model):
+    """[contact model]
+
+    Args:
+        models ([class]): [create contact form]
+
+    """
+    email = models.EmailField()
+    subject = models.CharField(max_length=255)
+    message = models.TextField()
+
+    def __str__(self):
+        return self.email
--- Basi/views.py
+++ Basi/views.py
@@ -1,8 +1,8 @@
-
+from django.contrib import messages
 from django.shortcuts import redirect, render
 from django.urls import reverse, reverse_lazy
 from django.views.generic import TemplateView
-
+from .forms import ContactForm
 # Create your views here.
 
 def home(request):
@@ -28,5 +28,58 @@ def index(request):
         request ([type]): [description]
     """
     return render(request, "home/index.html")
+
+
+
+
+from django.shortcuts import render
+from django.contrib import messages
+
+from django.conf import settings
+from mailchimp_marketing import Client
+from mailchimp_marketing.api_client import ApiClientError
+
+
+# Mailchimp Settings
+api_key = settings.MAILCHIMP_API_KEY
+server = settings.MAILCHIMP_DATA_CENTER
+list_id = settings.MAILCHIMP_EMAIL_LIST_ID
+
+
+# Subscription Logic
+def subscribe(email):
+    """
+     Contains code handling the communication to the mailchimp api
+     to create a contact/member in an audience/list.
+    """
+
+    mailchimp = Client()
+    mailchimp.set_config({
+        "api_key": api_key,
+        "server": server,
+    })
+
+    member_info = {
+        "email_address": email,
+        "status": "subscribed",
+    }
+
+    try:
+        response = mailchimp.lists.add_list_member(list_id, member_info)
+        print("response: {}".format(response))
+    except ApiClientError as error:
+        print("An exception occurred: {}".format(error.text))
+
+
+
+
+# Views here.
 def about(request):
+    if request.method == "POST":
+        email = request.POST['email']
+        subscribe(email)                    # function to access mailchimp
+        messages.success(request, "Email received. thank You! ") # message
+
     return render(request, "home/about.html")
+
+
